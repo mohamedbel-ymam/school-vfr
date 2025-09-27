4cd6c955ed39ac2682673c4c47e0f2bbffa8f223
mohamedbel-ymam <mohamed.belymam07@gmail.com>
2025-09-27 18:53:08 +0100

    cors modification

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => [ 'api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'connexion', 'deconnexion', 'déconnexion'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://takwaetablissement.com', 'https://www.takwaetablissement.com'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
